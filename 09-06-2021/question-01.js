1f482a02f13b1862513997dc091dcc8e97a25b88
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-06-11 16:16:22 +0530

    09-06-2021 exercises

---
+++ 09-06-2021/question-01.js
@@ -0,0 +1,14 @@
+let input = prompt("Enter an array Example: 1,2,3,4");
+let array = input.split(",");
+let revArray = [];
+let lenArray = array.length -1 ;
+
+// for (i = 1; i < array.length; i ++){
+//     revArray.push(array.slice(-i))
+// }
+while (revArray.length < array.length){
+    revArray.push(array[lenArray]);
+    lenArray --;
+}
+
+console.log(revArray);
