afb0ca4aaf77b788a1aaca5872eb05c8329f8101
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-06-11 22:33:23 +0530

    Update changes

--- 09-06-2021/question-01.js
+++ 09-06-2021/question-01.js
@@ -1,14 +1,18 @@
-let input = prompt("Enter an array Example: 1,2,3,4");
-let array = input.split(",");
-let revArray = [];
-let lenArray = array.length -1 ;
+function reverseArray(){
+    let input = prompt("Enter an array Example: 1,2,3,4");
+    let array = input.split(",");
+    let revArray = [];
+    let lenArray = array.length -1 ;
 
-// for (i = 1; i < array.length; i ++){
-//     revArray.push(array.slice(-i))
-// }
-while (revArray.length < array.length){
-    revArray.push(array[lenArray]);
-    lenArray --;
+    // for (i = 1; i < array.length; i ++){
+    //     revArray.push(array.slice(-i))
+    // }
+
+    while (revArray.length < array.length){
+        revArray.push(array[lenArray]);
+        lenArray --;
+    }
+
+    console.log(revArray);
 }
-
-console.log(revArray);
+reverseArray();
--- 09-06-2021/question-02.js
+++ 09-06-2021/question-02.js
@@ -1,22 +1,25 @@
-let input = prompt("Enter a array");
-let array = input.split(",");
-let nonDupArr = [];
-let count = 0;
+function elementCount(){
+    let input = prompt("Enter an array");
+    let array = input.split(",");
+    let nonDupArr = [];
+    let count = 0;
 
-for (i in array){
-    if (nonDupArr.includes(array[i]) == false){
-        nonDupArr.push(array[i]);
-    }else{
-        console.log(nonDupArr.includes(array[i]));
-    }
-}
-
-for (i in nonDupArr){
-    for (j in array){
-        if (nonDupArr[i] == array[j]) {
-            count = count + 1;
+    for (i in array){
+        if (nonDupArr.includes(array[i]) == false){
+            nonDupArr.push(array[i]);
+        }else{
+            console.log(nonDupArr.includes(array[i]));
         }
     }
-    console.log(`${nonDupArr[i]} = ${count}`);
-    count = 0;
-}
+
+    for (i in nonDupArr){
+        for (j in array){
+            if (nonDupArr[i] == array[j]) {
+                count = count + 1;
+            }
+        }
+        console.log(`${nonDupArr[i]} = ${count}`);
+        count = 0;
+    }
+}   
+elementCount();
