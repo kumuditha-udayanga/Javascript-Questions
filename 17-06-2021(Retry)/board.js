1983bc15595095a3c8603f8d3e35e7fd93a692b2
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-06-24 23:54:31 +0530

    Initiate chess pieces

---
+++ 17-06-2021(Retry)/board.js
@@ -0,0 +1,34 @@
+// const chessBoard = {
+//     a8: null, b8: null, c8: null, d8: null, e8: null, f8: null, g8: null, h8: null,
+//     a7: null, b7: null, c7: null, d7: null, e7: null, f7: null, g7: null, h7: null, 
+//     a6: null, b6: null, c6: null, d6: null, e6: null, f6: null, g6: null, h6: null,
+//     a5: null, b5: null, c5: null, d5: null, e5: null, f5: null, g5: null, h5: null,
+//     a4: null, b4: null, c4: null, d4: null, e4: null, f4: null, g4: null, h4: null,
+//     a3: null, b3: null, c3: null, d3: null, e3: null, f3: null, g3: null, h3: null,
+//     a2: null, b2: null, c2: null, d2: null, e2: null, f2: null, g2: null, h2: null,
+//     a1: null, b1: null, c1: null, d1: null, e1: null, f1: null, g1: null, h1: null
+// }
+
+// // Diagonal rules
+// const diagonals = {
+//     a:['b'],
+//     b:['a','c'],
+//     c:['b','d'],
+//     d:['c','e'],
+//     e:['d','f'],
+//     f:['e','g'],
+//     g:['f','h'],
+//     h:['g']
+// }
+import players from './pieces.js'
+
+let letterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+let chessBoard  = {}
+for (let i in letterArray){
+    for (var x = 1; x < 9; x++) {
+        chessBoard[letterArray[i]+x] = {};
+      }
+}
+console.log(chessBoard)
+
+console.log(players.whiteKing)
---
+++ 17-06-2021(Retry)/pieces.js
@@ -0,0 +1,95 @@
+// function Piece(color, type, currentPosition, movingPosition) {
+//     this.color = color;
+//     this.currentPosition = currentPosition;
+//     this.movingPosition = movingPosition;
+//     this.movingPatterns = [];
+// }  
+
+function King (color){
+    this.type = "King";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    this.movePatterns = ["any"]
+}
+
+function Queen (color){
+    this.type = "Queen";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    this.movePatterns = ["any"]
+}
+
+function Rook (color){
+    this.type = "Rook";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    this.movePatterns = ["vertical", "horizontal"]
+}
+
+function Knight (color){
+    this.type = "Knight";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    // Breadth First Search or BFS
+    movePatterns = ["Lway"]
+}
+
+function Bishop (color){
+    this.type = "Bishop";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    this.movePatterns = ["diagonal"]
+}
+
+function Pawn (color){
+    this.type = "Pawn";
+    this.color = color,
+    this.position,
+    this.movingPosition,
+    this.movePatterns = ["forward", "diagonal"];
+}
+
+export default function players(){
+    //White players
+    const whiteKing = new King('white');
+    const whiteQueen = new Queen('white');
+    const whiteRook_01 = new Rook('white');
+    const whiteRook_02 = new Rook('white');
+    const whiteBishop_01 = new Bishop('white');
+    const whiteBishop_02 = new Bishop('white');
+    const whiteKnight_01 = new Knight('white');
+    const whiteKnight_02 = new Knight('white');
+
+    const whitePawn_01 = new Pawn('white');
+    const whitePawn_02 = new Pawn('white');
+    const whitePawn_03 = new Pawn('white');
+    const whitePawn_04 = new Pawn('white');
+    const whitePawn_05 = new Pawn('white');
+    const whitePawn_06 = new Pawn('white');
+    const whitePawn_07 = new Pawn('white');
+    const whitePawn_08 = new Pawn('white');
+
+    //Black players
+    const blackKing = new King('black');
+    const blackQueen = new Queen('black');
+    const blackRook_01 = new Rook('black');
+    const blackRook_02 = new Rook('black');
+    const blackBishop_01 = new Bishop('black');
+    const blackBishop_02 = new Bishop('black');
+    const blackKnight_01 = new Knight('black');
+    const blackKnight_02 = new Knight('black');
+
+    const blackPawn_01 = new Pawn('black');
+    const blackPawn_02 = new Pawn('black');
+    const blackPawn_03 = new Pawn('black');
+    const blackPawn_04 = new Pawn('black');
+    const blackPawn_05 = new Pawn('black');
+    const blackPawn_06 = new Pawn('black');
+    const blackPawn_07 = new Pawn('black');
+    const blackPawn_08 = new Pawn('black');
+}
