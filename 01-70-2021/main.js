6c7664199533c1ab232bdd2dfd6f64ae1ea116ee
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-07-04 22:14:58 +0530

    Add payroll function

---
+++ 01-70-2021/main.js
@@ -0,0 +1,28 @@
+let company = (function(){
+    let salary;
+    let allowance;
+    // console.log(salary);
+    return{
+        director: function(){
+            salary = 300000;
+            allowance = 75000;
+            return salary + allowance;
+        },
+        manager: function(){
+            salary = 200000;
+            allowance = 50000;
+            return salary + allowance;
+        },
+        worker: function(){
+            salary = 100000;
+            allowance = 25000;
+            return salary + allowance;
+        }
+    };
+})();
+
+function print (place, designation){
+    console.log(place.designation);
+}
+
+print(company.director);
