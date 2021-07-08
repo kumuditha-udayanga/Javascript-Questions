23292b1e58fafce1a05297ba42edc7e0410a1649
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-07-08 23:19:52 +0530

    Remove commented statements

--- 01-70-2021/calculator.js
+++ 01-70-2021/calculator.js
@@ -4,9 +4,7 @@ divide = (number01) => (number02) => number01 / number02;
 multiply = (number01) => (number02) => number01 * number02;
 
 function calculate(operation, number01, number02){
-    // console.log(operation(number01)(number02));
     console.log(operation(number01, number02));
 }
 
-// calculate(add, 12, 14);
 calculate(subtract, 12, 14);
