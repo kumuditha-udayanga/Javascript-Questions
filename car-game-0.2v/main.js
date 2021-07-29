e21aaf6284a2c50d59b07c456363a140d9c5c4ba
kumuditha-udayanga <kumudithaudaya@gmail.com>
2021-07-29 23:33:59 +0530

    Add track lines

--- car-game-0.2v/main.js
+++ car-game-0.2v/main.js
@@ -1,8 +1,8 @@
 //creating a canvas using JS
 let canvas = document.createElement("canvas");
 //making the canvas fullscreen
-let w = canvas.width = 2245;
-let h = canvas.height = 1097;
+let w = canvas.width = innerWidth;
+let h = canvas.height = innerHeight;
 let ctx = canvas.getContext("2d");
 //appending the canvas to the body
 document.body.appendChild(canvas);
@@ -11,12 +11,33 @@ document.body.appendChild(canvas);
 // ctx.fillRect(1100, 500, 100, 10);
 // ctx.fillRect(1095, 510, 110, 10);
 // ctx.fillRect(1090, 520, 120, 10);
-roadLength = 1100;
-segmentSize = 500;
-segmentXpos = 100;
-while (roadLength > 0){
-    ctx.fillRect(roadLength, segmentSize, segmentXpos, 10);
-    roadLength -= 5;
-    segmentSize += 4;
-    segmentXpos += 10;
+gameComponents = {
+    drawTrack: () => {
+        segmentXpos = w/2;
+        segmentYpos = 500;
+        roadWidth = 40;
+        while (segmentXpos > 0){
+            ctx.fillRect(segmentXpos, segmentYpos, roadWidth, 10);
+            segmentXpos -= 5;
+            segmentYpos += 4;
+            roadWidth += 10;
+        }
+    },
+    drawTrackLines: () => {
+        segmentXpos = w/2 + 16;
+        segmentYpos = 500; 
+        ctx.fillStyle = "white";
+        ctx.fillRect(segmentXpos, 510, 5, 15);
+        
+        // ctx.fillRect(segmentXpos - 1, 540, 7, 15 + 2);
+        // ctx.fillRect(segmentXpos - 2, 570, 9, 15 + 4);
+        // ctx.fillRect(segmentXpos - 3, 600, 11, 15 + 8);
+        // ctx.fillRect(segmentXpos - 4, 640, 13, 15 + 16);
+        // ctx.fillRect(segmentXpos - 5, 700, 16, 15 + 32);
+        // ctx.fillRect(segmentXpos - 5, 800, 19, 15 + 64);
+        // ctx.fillRect(segmentXpos - 5, 960, 23, 15 + 128);
+    }
 }
+
+gameComponents.drawTrack();
+gameComponents.drawTrackLines();
